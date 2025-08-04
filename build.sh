77158e7a35b03774e92335443e169bed1894a7b1
RishabhVaishnav-star <rishabhvaishnav812@gmail.com>
2025-08-04 17:28:52 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Install system dependencies needed for compiling packages
-apt-get update -y
-apt-get install -y build-essential python3-dev
-
-# Then, proceed with the normal pip install
-pip install -r requirements.txt
